dcfa26d01c12958fcf208274dce816a7c34a08ed
juliana jones <juliana.p.jones@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email juliana.p.jones@gmail.com>
2021-04-27 19:12:27 -0400

    adding everything

---
+++ src/main/java/com/tech522/demoseeddata/DemoSeedDataApplication.java
@@ -0,0 +1,13 @@
+package com.tech522.demoseeddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSeedDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSeedDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech522/demoseeddata/DemoSeedDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech522.demoseeddata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSeedDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
